61bb84c83bc0f41ae597abad78645b6dbe9dab3f
Asif Shaik <90194646+asifshaik2230@users.noreply.github.com>
2022-08-17 18:13:29 +0530

    Delete paymentbank-backends directory

--- paymentbank-backends/src/main/java/com/dbs/paymentbank/Repository/MessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dbs.paymentbank.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dbs.paymentbank.Model.MessageCode;
-
-public interface MessageRepository extends JpaRepository<MessageCode, String> {
-
-}
--- paymentbank-backends/src/main/java/com/dbs/paymentbank/Repository/SanctionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dbs.paymentbank.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dbs.paymentbank.Model.SanctionList;
-
-public interface SanctionRepository extends JpaRepository<SanctionList, String>{
-
-}
