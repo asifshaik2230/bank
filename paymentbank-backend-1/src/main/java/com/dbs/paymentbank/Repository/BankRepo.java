692260ec568fb98e4ff6c5bd49ada53de54defb5
Asif Shaik <90194646+asifshaik2230@users.noreply.github.com>
2022-08-17 18:13:17 +0530

    Delete paymentbank-backend-1 directory

--- paymentbank-backend-1/src/main/java/com/dbs/paymentbank/Repository/BankRepo.java
+++
@@ -1,9 +0,0 @@
-package com.dbs.paymentbank.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.dbs.paymentbank.Model.BankBic;
-
-public interface BankRepo extends JpaRepository<BankBic, String> {
-
-}
